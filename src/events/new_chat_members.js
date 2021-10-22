6139f0474f70261b6d10f6a32ed83d66e22c959a
Reece <business@reecebenson.me>
2021-10-22 23:50:35 +0000

    new member event

---
+++ src/events/new_chat_members.js
@@ -0,0 +1,14 @@
+class NewMemberEvent {
+  constructor(bot) {
+    this.bot = bot;
+    this.meta = {
+      name: "new_chat_members"
+    };
+  }
+
+  async run(ctx) {
+    console.log('Found new chat member:', ctx);
+  }
+};
+
+module.exports = NewMemberEvent;
