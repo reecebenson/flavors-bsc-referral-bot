65852acc6e268fed3ffa9c1c58ff4b151746313f
Reece <business@reecebenson.me>
2021-10-24 02:28:56 +0000

    add points command

--- src/commands/points.js
+++ src/commands/points.js
@@ -9,7 +9,25 @@ module.exports = class {
     };
   }
 
+  async getMyPoints(uid) {
+    const db = this.bot.db.fetch();
+    const userRefs = db.collection('user_references');
+    let points = 0;
+
+    const referrals = await userRefs.find({ userId: uid }).toArray();
+    if (referrals.length === 0) return 0;
+
+    // Work out how many points this user has
+    const mine = referrals.shift();
+    points = (mine.referrals || []).length;
+    mine.referrals.forEach((referral) => {
+      points += referral.messageCount;
+    });
+    return points;
+  }
+
   async run(msg) {
-    this.bot.sendMessage(msg.chat.id, `You have 0 points.`);
+    const points = await this.getMyPoints(msg.from.id);
+    this.bot.sendMessage(msg.chat.id, `You have ${points} point${points === 1 ? '' : 's'}.`);
   }
 };
